077dc0403e28b118cb248c0e4189fff9e91955db
Rico Putra Pradana <pradanaricoputra@gmail.com>
2025-04-07 05:46:50 +0700

    feat: implement a function for retrieving USER_AUTH_METHOD records by auth_identifier

---
+++ src/db/user_auth_method.ts
@@ -0,0 +1,29 @@
+import { NeonHttpQueryResult } from "drizzle-orm/neon-http";
+import { TUserAuthMethodSelect } from "./schema";
+import db from "@/clients/db";
+
+/**
+ * Retrieves all user authentication methods associated with the given identifier.
+ *
+ * @param auth_identifier - The authentication identifier (e.g. email, Google ID, etc.)
+ * @returns A list of user authentication methods associated with the given identifier.
+ *
+ * @throws Error - If there is an error during the query execution.
+ */
+export async function getUserAuthMethods(
+  auth_identifier: string
+): Promise<TUserAuthMethodSelect[]> {
+  try {
+    const result: NeonHttpQueryResult<TUserAuthMethodSelect> = await db.execute(`
+      SELECT *
+      FROM user_auth_method
+      WHERE auth_identifier = '${auth_identifier}'
+    `);
+    return result.rows;
+  }
+  catch (error) {
+    // log error properly
+    console.error("[auth.ts] getUserAuthMethods - error:", error);
+    return []
+  }
+}
